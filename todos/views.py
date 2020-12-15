2abcef17a449252b4c4068876df50a6036b8a006
kaigikinyua <kaigikinyua@gmail.com>
2020-12-15 10:36:06 +0300

    init

---
+++ todos/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views import View
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+class Login(View):
+    def get(request):
+        return render(request,"login.html")
+    def post(request):
+        return render(request,"login.html")
+
+class SignUp(View):
+    def get(request):
+        return render(request,"signup.html")
+    def post(request):
+        return render(request,"signup.html")
+
+def userpage(request):
+    return render(request,"userpage.html")
+
+class Todo(View):
+    def get(request,operation,todoid):
+        return render(request,"todo.html")
+    def post(request,operation,todoid):
+        return render(request,"todo.html")
