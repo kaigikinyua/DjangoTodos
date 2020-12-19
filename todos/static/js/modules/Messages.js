547bb2d90c3feb1f1b13d51ba245237c291990ab
kaigikinyua <kaigikinyua@gmail.com>
2020-12-19 20:42:37 +0300

    JS init

--- todos/static/js/modules/Messages.js
+++ todos/static/js/modules/Messages.js
@@ -5,3 +5,4 @@ class Messages{
     }
 
 }
+export {Messages}
--- todos/static/js/modules/UI.js
+++ todos/static/js/modules/UI.js
@@ -1,13 +1,33 @@
+import {Messages} from './Messages.js';
 class DynamicUI{
-    parent="";child=""
-    constructor({parentID,contents}){
-        this.parent=parentID;this.contents=contents
+    parent="";
+    constructor({parentID}){
+        this.parent=parentID;
     }
-    appendElement(){
+    appendElement(child){
         var parent_elm=document.getElementById(this.parent)
         if(parent_elm!=null && parent_elm!=undefined){
-            parent_elm.appendChild(this.child)
+            parent_elm.appendChild(child)
             return true
         }else{return false}
     }
+    hideElement(){
+        if(elem_exists(this.parent)){
+            var elem=document.getElementById(this.parent)
+            elem.style.display="none"
+        }
+        
+    }
 }
+
+function elem_exists(elem){
+    var elem=document.getElementById(elem)
+    if(elem!=undefined && elem!=null){
+        return true
+    }else{
+        Messages.showError("Element by id "+elem+" does not exist");
+        return false
+    }
+}
+
+export {DynamicUI}
