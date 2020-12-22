b3f92402f34253ea891f0ca6831474db29c825a3
kaigikinyua <kaigikinyua@gmail.com>
2020-12-22 22:19:19 +0300

    testing views

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
 # Create your tests here.
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.client=Client()
+
+    def test_indexpage(self):
+        response=self.client.get("/")
+        response_check(response)
+        self.assertEqual(response.status_code,200)
+
+    def test_loginpage(self):
+        response=self.client.get("/login")
+        self.assertEqual(response.status_code,200)
+    
+    def test_signup(self):
+        response=self.client.get("/signup")
+        self.assertEqual(response.status_code,200)
+
+#class TodosDBTest(TestCase)
